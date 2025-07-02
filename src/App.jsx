aae7f3fb57e46751d421dca3b8ea55c92beccdfb
bruno.silveira <brunosilveira@gentecontrole.com.br>
2025-07-02 18:14:06 -0300

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import './App.scss'
-import { useState } from 'react'
+// import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header.jsx'
-import Nav from './components/Nav.jsx'
 import Section from './components/Section.jsx'
 import Footer from './components/Footer.jsx'
 import Find from './view/Find'
 import Insert from './view/Insert'
+import Init from './view/Init.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Nav />
         <Section>
           <Routes>
+            <Route path="/" element={<Init />} />
             <Route path="/find" element={<Find />} />
             <Route path="/insert" element={<Insert />} />
           </Routes>
         </Section>
         <Footer />
       </BrowserRouter>
+      
     </>
   )
 }
-
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
-import { Link } from 'react-router-dom';
-import './../style/Sass.scss'
+import { Link } from "react-router-dom";
+import ReactLogo from "../assets/reactjs-line.png";
+import "./../style/Sass.scss";
+import Nav from './Nav.jsx'
 
 export default function Header() {
-    return (
-        <>
-            <header>
-                <Link to="/" id="logo">LOGO</Link>
-            </header>
-        </>
-    )
+  return (
+    <>
+      <div id="top-bar">
+        <header>
+          <Link to="/" id="logo">
+            <img src={ReactLogo} alt="logo" />
+          </Link>
+        </header>
+        <Nav />
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/find">Buscar</Link>
+            </li>
+            <li>
+              <Link to="/insert">Inserir</Link>
+            </li>
+          </ul>
+        </nav> */}
+      </div>
+    </>
+  );
 }
